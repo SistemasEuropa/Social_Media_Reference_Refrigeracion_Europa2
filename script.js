3d822bde1ea25b08f8d704c534b0fad398054a76
Caranfa11 <luiscaranfa1@gmail.com>
2022-11-02 12:12:18 -0400

    try to lock screen-rotation, bad news for now

--- script.js
+++ script.js
@@ -16,10 +16,8 @@ let btn = document.getElementById("location");
 let popup = document.getElementById("popup");
 let body = document.getElementById("body");
 let footer = document.getElementById("footer")
-let header  = document.getElementById("header");
-let logo_popup = document.getElementById("logo-popup")
+let header = document.getElementById("header");
+let logo_popup = document.getElementById("logo-popup");
 
 btn.addEventListener("click", show_popup_location);
 logo_popup.addEventListener("click", hide_popup_location);
-
-
