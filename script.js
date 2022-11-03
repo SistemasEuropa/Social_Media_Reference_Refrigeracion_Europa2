1d1674239c308628865ca23416b13f98750478b0
Caranfa11 <luiscaranfa1@gmail.com>
2022-11-03 14:19:55 -0400

    Add 2 animation to the address btn, now he goes dark and light when the popup appear...that's crazy men i like it

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+let sleep = ms => {
+    return new Promise (resolve=>setTimeout(resolve, ms));
+}
+
 function show_popup_location(){
-    popup.classList.remove("hide");
-    body.classList.add("hide");
-    header.classList.add("hide");
-    footer.classList.add("hide");
+    all_body.classList.add("blur");
+    all_body.classList.remove("reverse-blur")
+    sleep(250).then(()=>{
+        body.classList.add("hide");
+        header.classList.add("hide");
+        footer.classList.add("hide");
+        popup.classList.remove("hide");
+    });
+    
 };
 
 function hide_popup_location(){
-    popup.classList.add("hide");
-    body.classList.remove("hide");
-    header.classList.remove("hide");
-    footer.classList.remove("hide");
+    all_body.classList.add("reverse-blur");
+    all_body.classList.remove("blur");
+    sleep(250).then(()=>{
+        popup.classList.add("hide");
+        body.classList.remove("hide");
+        header.classList.remove("hide");
+        footer.classList.remove("hide");
+    });
 }
 
+let all_body = document.getElementById("all");
 let btn = document.getElementById("location");
 let popup = document.getElementById("popup");
 let body = document.getElementById("body");
