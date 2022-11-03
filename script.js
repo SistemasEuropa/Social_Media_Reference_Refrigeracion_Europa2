5fc345594771e052862b7cfbd9805b67a3b730a9
Caranfa11 <luiscaranfa1@gmail.com>
2022-11-03 15:11:23 -0400

    slow motion of animation

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ let sleep = ms => {
 }
 
 function show_popup_location(){
-    all_body.classList.add("blur");
     all_body.classList.remove("reverse-blur")
-    sleep(250).then(()=>{
+    all_body.classList.add("blur");
+    sleep(400).then(()=>{
         body.classList.add("hide");
         header.classList.add("hide");
         footer.classList.add("hide");
         popup.classList.remove("hide");
     });
-    
+   
 };
 
 function hide_popup_location(){
-    all_body.classList.add("reverse-blur");
     all_body.classList.remove("blur");
-    sleep(250).then(()=>{
+    all_body.classList.add("reverse-blur");
+    sleep(400).then(()=>{
         popup.classList.add("hide");
         body.classList.remove("hide");
         header.classList.remove("hide");
         footer.classList.remove("hide");
     });
-}
+};
 
 let all_body = document.getElementById("all");
 let btn = document.getElementById("location");
